a8f12fe4134f78b51300ce5b00d495f34e0b2ecd
兮尘 <761516186@qq.com>
2021-09-23 20:21:16 +0800

    upload by parts

---
+++ public/preload.js
@@ -0,0 +1,33 @@
+window.exports = {
+  torch: {
+    mode: 'doc',
+    args: {
+      // indexes: require('./indexes.json')
+      indexes: [
+        { t: "master (unstable)", d: "", p: "docs/master/index.html" },
+        { t: "v1.9.1 (stable release)", d: "稳定版", p: "docs/1.9.1/index.html" },
+        { t: "v1.9.0", d: "", p: "docs/1.9.0/index.html" },
+        { t: "v1.8.1", d: "", p: "docs/1.8.1/index.html" },
+        { t: "v1.8.0", d: "", p: "docs/1.8.0/index.html" },
+        { t: "v1.7.1", d: "", p: "docs/1.7.1/index.html" },
+        { t: "v1.7.0", d: "", p: "docs/1.7.0/index.html" },
+        { t: "v1.6.0", d: "", p: "docs/1.6.0/index.html" },
+        { t: "v1.5.1", d: "", p: "docs/1.5.1/index.html" },
+        { t: "v1.5.0", d: "", p: "docs/1.5.0/index.html" },
+        { t: "v1.4.0", d: "", p: "docs/1.4.0/index.html" },
+        { t: "v1.3.1", d: "", p: "docs/1.3.1/index.html" },
+        { t: "v1.3.0", d: "", p: "docs/1.3.0/index.html" },
+        { t: "v1.2.0", d: "", p: "docs/1.2.0/index.html" },
+        { t: "v1.1.0", d: "", p: "docs/1.1.0/index.html" },
+        { t: "v1.0.1", d: "", p: "docs/1.0.1/index.html" },
+        { t: "v1.0.0", d: "", p: "docs/1.0.0/index.html" },
+        { t: "v0.4.1", d: "", p: "docs/0.4.1/index.html" },
+        { t: "v0.4.0", d: "", p: "docs/0.4.0/index.html" },
+        { t: "v0.3.1", d: "", p: "docs/0.3.1/index.html" },
+        { t: "v0.3.0", d: "", p: "docs/0.3.0/index.html" },
+        { t: "v0.2.0", d: "", p: "docs/0.2.0/index.html" },
+        { t: "v0.1.12", d: "", p: "docs/0.1.12/index.html" },
+      ],
+    }
+  }
+}
